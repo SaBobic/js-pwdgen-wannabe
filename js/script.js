c683b36784121e1fbbd614fc4d298858699a675a
SaBobic <fabrizioettori@gmail.com>
2022-06-20 17:40:19 +0200

    added short script version

--- js/script.js
+++ js/script.js
@@ -7,27 +7,29 @@
 - Stampare output nomecognomecolorepreferito21 sulla pagina
 */
 
-// Creare variabile per id=password
+// *** Long script ***
 
 const password = document.getElementById('password');
 
-// Creare variabile per numero da aggiungere
-
 const randomNumber = '21';
-
-// Chiedere all’utente il nome
 
 const firstName = prompt('Qual è il tuo nome?');
 
-// Chiedere all’utente il nome cognome
-
 const lastName = prompt('Qual è il tuo cognome?');
-
-// Chiedere all’utente il nome il colore preferito
 
 const color = prompt('Qual è il tuo colore preferito?');
 
-// Stampare output nomecognomecolorepreferito21 sulla pagina
-
 const passwordGenerator = firstName + lastName + color + randomNumber;
 password.innerHTML=`<strong>${passwordGenerator}</strong>`;
+
+// *** Short script ***
+
+/*
+const firstName = prompt('Qual è il tuo nome?');
+
+const lastName = prompt('Qual è il tuo cognome?');
+
+const color = prompt('Qual è il tuo colore preferito?');
+
+document.getElementById('password').innerHTML=`<strong>${firstName}${lastName}${color}21</strong>`;
+*/
